beba989b3f0a577bde5cf293897a635214d5a3aa
Niklasodell <144327353+Niklasodell@users.noreply.github.com>
2023-09-18 11:46:32 +0200

    Add files via upload

---
+++ Lektion_3_ovning/src/com/niklas/ovning3/Main.java
@@ -0,0 +1,22 @@
+package com.niklas.ovning3;
+
+import com.niklas.ovning3.student.Student;
+
+import javax.xml.namespace.QName;
+
+public class Main {
+    public static void main(String[] args) {
+/*
+        Student filip = new Student("Filip", 15);
+*/
+
+        for (int i = 0; i <4; i++) {
+            System.out.println();
+        }
+        for (String i = String.valueOf(0); i < ; i++) {
+            System.out.println();
+
+        }
+        }
+
+}
